0a9adf6c77172409ade6a3488507ec7677b75dfe
#nachimak <nachimak28@gmail.com>
2019-03-07 16:03:02 +0530

    hardcode shit - p1

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <p>List</p>
+      <input type="checkbox" value="1" name="1" />
+      <span>Option 1</span>
+      <br />
+      <input type="checkbox" value="2" name="2" />
+      <span>Option 2</span>
+      <br />
+      <input type="checkbox" value="3" name="3" />
+      <span>Option 3</span>
+      <br />
+      <input type="checkbox" value="4" name="4" />
+      <span>Option 4</span>
+      <br />
+      <input type="checkbox" value="5" name="5" />
+      <span>Option 5</span>
+      <br />
+    </div>
+  );
+}
+
+export default App;
